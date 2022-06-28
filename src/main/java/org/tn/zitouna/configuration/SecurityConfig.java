cf2669d73dfe584801da7ef541be190efa202233
MedFarah <hamouchka7@gmail.com>
2022-06-28 17:43:22 +0200

    monitor microservice

--- src/main/java/org/tn/zitouna/configuration/SecurityConfig.java
+++ src/main/java/org/tn/zitouna/configuration/SecurityConfig.java
@@ -31,9 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/actuator/**").permitAll();
 		http.csrf().disable().cors().configurationSource(corsConfigurationSource()).and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().authorizeRequests().antMatchers("/login").permitAll()
+		.and().authorizeRequests().antMatchers("/actuator/**").permitAll()
 		.anyRequest().authenticated().and().addFilterBefore(this.jwtTokenAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 		
 	}
